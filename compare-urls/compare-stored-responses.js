bdcd5450ee3cf31407aa9972a80ca3b61494771b
philjhanna <Phil@Philips-MacBook-Air.local>
2016-12-27 19:58:56 -0600

    steading improving

--- compare-urls/compare-stored-responses.js
+++ compare-urls/compare-stored-responses.js
@@ -3,24 +3,58 @@ var diff = require('diff'),
 	directory = './output/',
 	firstResponseHeader = null; //this last one is ugle. nasty pattern.
 
+var compareObj = function(requestAll1, requestAll2) { 
+	var identical = true;
+	//I have assumed (yup) that 
+	requestDetails1 = requestAll1[0];
+	requestDetails2 = requestAll2[0];
+	responseHeaders1 = requestAll1[1];
+	responseHeaders2 = requestAll2[1];
+	if(requestDetails1['details']['path'] != requestDetails2['details']['path']) {
+		console.log("CRITICAL ERROR: assumed paths would be the same but they are not. We aren't comparing urls that just differ by domain.  Could be ordering maybe. " + 
+			JSON.stringify(requestDetails1['details']) + ',' + JSON.stringify(requestDetails2['details']));
+	} else {
+		console.log('NEW COMPARISON ' + requestDetails1['details']['path'] + ',' + requestDetails1['details']['domain'] + ',' + requestDetails2['details']['domain']);
+		for (var key in responseHeaders1) {//not, currently missing iteration ove responseHeaders2
+			if (responseHeaders1.hasOwnProperty(key) && responseHeaders2.hasOwnProperty(key)) {
+				if(responseHeaders1[key] != responseHeaders2[key]){
+					console.log('DIFFERENCE: ' + key + ':' + responseHeaders1[key] + ',' + responseHeaders2[key]);
+					identical = false;
+				}
+			} else if(responseHeaders1.hasOwnProperty(key)) {
+				console.log('DIFFERENCE In 1 but not 2 ' + key + ':' + responseHeaders1[key] );
+				identical = false;
+			} else if(responseHeaders2.hasOwnProperty(key)) {
+				console.log('DIFFERENCE In 2 but not 1 ' + key + ':' + responseHeaders2[key] );
+				identical = false;
+			} else {
+				console.log("shouldn't be possible. Neither has a value for a value found.");
+			}
+	    }
+	    if(identical){
+	    	console.log('RESPONSES IDENTICAL');    	
+	    }
+	}
+}; 
+
 fs.readdir(directory, (err, files) => {
+	var firstFile = true;
 	files.forEach(file => {
 		//I'm assuming a certain order which is bad
 		//and this is an ugly way to do this.
 		//but i'm trying to avoid building a large object in memory for now so...
-		domainIndex = parseInt(file.substring(file.lastIndexOf('-') + 1)); //assume less than 10 domains
-		console.log(file + ' ' + domainIndex);
-		if(domainIndex == 0){
-			console.log('attempt read : ' + directory + file);
+		var fileLookingFor = file.indexOf('details_') == 0;
+		//parseInt(file.substring(file.lastIndexOf('_') + 1)); //assume less than 10 domains and later specifically 2
+		if (!fileLookingFor){
+			console.log('file not as expected. Ignoring : ' + file);
+		} else if(firstFile){
 			firstResponseHeader = fs.readFileSync(directory + file);
-		} else if(domainIndex == 1){
+			firstFile = false;
+		} else if(!firstFile){
 			var secondResponseHeader = fs.readFileSync(directory + file);
-			console.log('firstResponseHeader: ' + firstResponseHeader);
-			console.log('secondResponseHeader: ' + secondResponseHeader);
-			var differences = diff.diffJson(firstResponseHeader, secondResponseHeader);
-			console.log(differences);
- 		} else {
-			console.error('file not as expected : ' + file);
-		}
+			var differences = compareObj(JSON.parse(firstResponseHeader), JSON.parse(secondResponseHeader));
+			firstFile = true;
+ 		}
 	});
 })
+
--- compare-urls/store-responses.js
+++ compare-urls/store-responses.js
@@ -8,21 +8,27 @@ var files = [],
 		'/stocks.html'
 	],
 	domains = [
-		'http://www-stg.morningstar.com',
-		'http://www-qa.morningstar.com'
+		'http://beta-stg.morningstar.com',
+		'http://beta-qa.morningstar.com'
 	];
 
+function hashCode(str) {
+  return str.split('').reduce((prevHash, currVal) =>
+    ((prevHash << 5) - prevHash) + currVal.charCodeAt(0), 0);
+}
 
 paths.forEach(function (paths, indexPaths) {
 	domains.forEach(function (domains, indexDomains) {
 		needle.get(domains + paths, function (err, resp, body) {
-			var name = directory + indexPaths + '-' + indexDomains + '.txt';
+			//note: this loop is asynch. These get's will likely not return in the order they were sent.
+			var descJSON = '[{"details":{"domain":"' + domains + '","path":"' + paths + '"}}]';
+			var name = directory + 'details_' + hashCode(paths) + '_' + hashCode(domains) + '.txt';
 			console.log(name + " " + domains + paths);
-			fs.writeFile(name, JSON.stringify(resp.headers), function (fErr) {});
+			fs.writeFile(name, JSON.stringify(JSON.parse(descJSON).concat(resp.headers)), function (fErr) {});
 			files.push(name); //wathc the memory here, could get big and so far we don't need it.
 		});
 	});
 
-
 });
+
 
